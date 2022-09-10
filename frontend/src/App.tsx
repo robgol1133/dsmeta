2af5ba5505a184aca3a082b819dca5d28e4dac78
unknown <Robgol@CS-Robgol.softsite.private>
2022-09-10 13:47:21 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import NotificantionButton from "./components/NotificationButton"
+
 function App() {
   return (
-    <h1>Olá mundo</h1>
+    <>
+      <h1>Olá mundo</h1>
+      <NotificantionButton />
+    </>
   )
+
 }
 
 export default App
